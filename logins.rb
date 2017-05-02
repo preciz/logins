102876a24f7aa7e9e2e86155b515b89b0cec4a3d
preciz <barna.kovacs.codes@gmail.com>
2017-05-02 13:07:14 +0200

    Initial commit

---
+++ logins.rb
@@ -0,0 +1,12 @@
+def rand_time
+  Time.at(rand * Time.now.to_i)
+end
+
+persons = [[2, 'matayo'], [1, 'nico'], [0, 'angelo'], [3, 'luca']]
+
+logins = { # ugly on purpose
+  0 => [rand_time, rand_time, rand_time, rand_time, rand_time, rand_time, rand_time, rand_time],
+  1 => [rand_time, rand_time, rand_time, rand_time, rand_time, rand_time, rand_time, rand_time],
+  2 => [rand_time, rand_time, rand_time, rand_time, rand_time, rand_time, rand_time, rand_time],
+  3 => [rand_time, rand_time, rand_time, rand_time, rand_time, rand_time, rand_time, rand_time],
+}
